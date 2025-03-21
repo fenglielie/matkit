9af4c366de968abbac690209d0ccc3ffb3d2b442
fenglielie <fenglielie@qq.com>
2025-03-21 21:40:10 +0800

    feat(burgers_sin_exact): improve code quality and functionality
    - Refactor burgers_sin_exact.m for better readability and efficiency
    - Add argument validation and detailed comments
    - Update demo.m to include more time points and reference solutions
    - Improve plot visualization and add legend

--- base/burgers_sin_exact/burgers_sin_exact.m
+++ base/burgers_sin_exact/burgers_sin_exact.m
@@ -1,25 +1,44 @@
-% Exact solution of Burgers' equation with 2pi periodic boundary condition
-% u_t + u*u_x = 0
-% u0(x) = alpha + beta sin(x) in [-pi, pi]
-
 function u = burgers_sin_exact(x, t, alpha, beta)
-    %  u(x, 0) = alpha + beta*sin(x)
-    u = alpha + beta * burgers_sin_newton(x - alpha*t, beta*t);
+    % BURGERS_SIN_EXACT: Computes the exact solution of Burgers' equation (u_t + u * u_x = 0)
+    %       with 2pi periodic boundary condition at time t.
+    %       u(x, 0) = alpha + beta * sin(x) in [-pi, pi]
+    %
+    % Parameters:
+    %   x     - Spatial coordinate (matrix, vector, or scalar), numeric
+    %   t     - Time (scalar), numeric and non-negative
+    %   alpha - Scalar, numeric
+    %   beta  - Scalar, numeric
+    %
+    % Returns:
+    %   u - Solution of Burgers' equation at (x, t). size(u) == size(x)
+
+    arguments
+        x (:,:) double
+        t (1,1) double {mustBeNonnegative}
+        alpha (1,1) double
+        beta (1,1) double
+    end
+
+    % Compute the solution using the Newton iteration method
+    u = alpha + beta * burgers_sin_newton(x - alpha * t, beta * t);
 end
 
 
 function u = burgers_sin_newton(x, t)
+    % u_t + u u_x = 0, x in [-pi, pi]
     % u(x, 0) = sin(x)
-    x = mod(x + pi, 2*pi) - pi; % x in [-pi, pi]
 
-    iter = 1;
-    u = x/(pi/2+t); % u0
-    while iter < 1e5
-        du = (u-sin(x-u*t))./(1 + cos(x - u*t)*t);
+    x = mod(x + pi, 2 * pi) - pi;
+
+    % For t >= Tb, the algorithm becomes highly sensitive to the choice of the initial value for iteration.
+    u = x / (pi / 2 + t);
+
+    % Newton iteration
+    for iter = 1:1e5
+        du = (u - sin(x - u * t)) ./ (1 + cos(x - u * t) * t);
         u = u - du;
         if max(abs(du)) < 1e-10
             break
         end
-        iter = iter + 1;
     end
 end
--- base/burgers_sin_exact/demo.m
+++ base/burgers_sin_exact/demo.m
@@ -26,3 +26,30 @@ for t = [0.4, 1.0]
     plot(x, u_exact)
     hold off
 end
+
+
+xleft = 0;
+xright = 2*pi;
+nx = 200;
+
+figure;
+hold on
+
+for t = [0, 0.4, 2.0, 3.0]
+    [x, dx] = mesh_init_1d(xleft, xright, nx);
+
+    alpha = 0;
+    beta = 1;
+
+    if t > 1/abs(beta)
+        u0 = @(s) alpha + beta * sin(s);
+        plot(x + u0(x) * t, u0(x), LineStyle='--', DisplayName = sprintf("t = %.2f (ref)", t));
+    end
+
+    u_exact = burgers_sin_exact(x, t, alpha, beta);
+    plot(x, u_exact, LineWidth = 2, DisplayName = sprintf("t = %.2f", t))
+end
+
+xlim([xleft, xright])
+legend();
+hold off
